433cdb7e0a16e91af004660324d3e6e338784033
indymnv <indy.mnv@gmail.com>
2023-01-01 21:10:28 +0900

    add some code to read file from web

--- src/model.jl
+++ src/model.jl
@@ -1,9 +1,18 @@
 using MLJ
 using DataFrames
 using Plots
+using ZipFile
+using UrlDownload
+using HTTP
+
 include("src/read.jl")
 
 df = read_data()
 
+#data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+#f = download(data_url)
+#z = ZipFile.Reader(f)
+#z_by_filename = Dict( f.name => f for f in z.files)
+#df = CSV.read(z_by_filename["occurrence.txt"], DataFrame)
 
 
