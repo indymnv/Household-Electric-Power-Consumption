0e6bcc320e0d114a663b80feb8e5039bea919c86
indymnv <indy.mnv@gmail.com>
2023-01-02 12:24:03 +0900

    dataset load

--- src/model.jl
+++ src/model.jl
@@ -7,12 +7,13 @@ using HTTP
 
 include("src/read.jl")
 
-df = read_data()
+#df = read_data()
 
-#data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
-#f = download(data_url)
-#z = ZipFile.Reader(f)
-#z_by_filename = Dict( f.name => f for f in z.files)
-#df = CSV.read(z_by_filename["occurrence.txt"], DataFrame)
+data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+f = download(data_url)
+z = ZipFile.Reader(f)
+
+z_by_filename = Dict( f.name => f for f in z.files)
+df = CSV.read(z_by_filename["household_power_consumption.txt"], DataFrame)
 
 
